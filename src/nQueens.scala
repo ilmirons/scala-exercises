1b18a3f2956adc2915708f416b67a32f11c1f5a7
Andrea Mirone <andreamirone@gmail.com>
2014-03-20 01:00:33 +0100

    [nQueens] extracted trait

--- src/nQueens.scala
+++ src/nQueens.scala
@@ -1,18 +1,53 @@
 import scala.collection.immutable.Nil
 
-object nQueens extends App {
+trait nQueen {
+  
+  type Position
+  // TODO make different implementation of Board possible
+  type Board <: Seq[Position]
+  type Solution
+  
+  def mkBoard(n: Int): Board
+  
+  def updateAvailablePositions[T <: Seq[Position]](pos: Position, board: T): Seq[Position]
+  
+  def updateSolution(sol: Solution)(p: Position): Solution
+  
+  def printSolution(s: Solution) = print _
+  
+  def solve[T <: Seq[Position]](available: T, queens: Int,
+	    sol: Option[Solution]): Option[Solution] = {
+	  
+	  if (queens == 0) sol else
+	  if (available.length == 0) None else {
+		  solve(updateAvailablePositions(available.head, available), 
+		        queens - 1, 
+		        sol.map(updateSolution(_)(available.head))) match {
+		    case Some(solution) => Some(solution)
+		    case None => solve(available.tail, queens, sol)
+		  }
+	  }
+	}
+}
+
+object nQueens extends App with nQueen {
 
 	val N = args(0) toInt
 	
 	type Position = (Int, Int)
-	type Board = List[Position]
+	type Board    = List[Position]
+	type Solution = Board
 	
-	val board = (for {
-		i <- 0 until N 
-		j <- 0 until N
-	} yield (i, j)) toList
+	val board = mkBoard(N)
 	
-	def updateAvailable (pos: Position, board: Board) = {
+	def mkBoard(n: Int) = {
+		(for {
+			i <- 0 until N 
+			j <- 0 until N
+		} yield (i, j)) toList 
+	}
+	
+	def updateAvailablePositions[T <: Seq[Position]] (pos: Position, board: T) = {
 	  board.filter( _ match {
 	    case (pos._1, _) => false
 	    case (_, pos._2) => false
@@ -21,21 +56,12 @@ object nQueens extends App {
 	  })
 	}
 	
-	def solve(available: Board, queens: Int,
-	    solution: Option[Board] = Some(Nil)): Option[Board] = {
-	  
-	  if (queens == 0) solution else
-	  if (available.length == 0) None else {
-		  solve(updateAvailable(available.head, available), 
-		        queens - 1, 
-		        solution.map((sol) => available.head :: sol)) match {
-		    case Some(board) => Some(board)
-		    case None => solve(available.tail, queens, solution)
-		  }
-	  }
+	def updateSolution(sol: Solution)(p: Position): Solution = {
+	  p :: sol
 	}
 	
-	print(solve(board, N))
+	solve(board, N, Some(Nil)) match {
+	  case None           => print("No solution found :(")
+	  case Some(solution) => printSolution(solution)
+	}
 }
-
-
